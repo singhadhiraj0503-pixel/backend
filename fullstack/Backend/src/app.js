617163c38bcef8bf48d70154710d6a759c72c71f
singhadhiraj0503-pixel <singhadhiraj0503@gmail.com>
2026-04-30 00:03:58 +0530

    learning-4

--- fullstack/Backend/src/app.js
+++ fullstack/Backend/src/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const noteModel = require("./model/note.model");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("./public"));
 
 app.post("/api/notes", async (req, res) => {
   const { title, description } = req.body;
@@ -42,12 +44,16 @@ app.delete("/api/notes/:id", async (req, res) => {
 
 app.patch("/api/notes/:id", async (req, res) => {
   const id = req.params.id;
-  const { description } = req.body;
-  await noteModel.findByIdAndUpdate(id, { description });
+  const { title, description } = req.body;
+  await noteModel.findByIdAndUpdate(id, { title, description });
 
   res.status(200).json({
     message: "Note Updated Successfully",
   });
+});
+
+app.use("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
 });
 
 module.exports = app;
--- fullstack/Frontend/front-end/src/App.jsx
+++ fullstack/Frontend/front-end/src/App.jsx
@@ -2,43 +2,110 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [notes, setnotes] = useState([
-    {
-      title: "test title 1",
-      description: "test description",
-    },
-    {
-      title: "test title 2",
-      description: "test description",
-    },
-    {
-      title: "test title 3",
-      description: "test description",
-    },
-    {
-      title: "test title 4",
-      description: "test description",
-    },
-  ]);
+  const [notes, setnotes] = useState([]);
 
-  const information = async () => {
+  const fetchNotes = async () => {
     let { data } = await axios.get("http://localhost:3000/api/notes");
 
     setnotes(data.notes);
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const { title, description } = e.target.elements;
+    console.log(title.value, description.value);
+
+    const postNotes = async () => {
+      let { data } = await axios.post("http://localhost:3000/api/notes", {
+        title: title.value,
+        description: description.value,
+      });
+      console.log(data);
+      fetchNotes();
+    };
+    postNotes();
+  };
+
+  const deleteHandler = (noteId) => {
+    const deleteNote = async () => {
+      const { data } = await axios.delete(
+        "http://localhost:3000/api/notes/" + noteId,
+      );
+      console.log(data);
+      fetchNotes();
+    };
+    deleteNote();
+  };
+
+  const updateHandler = (noteId) => {
+    const newTitle = prompt("Enter new title:");
+    const newDescription = prompt("Enter new description");
+    const updateNote = async () => {
+      let { data } = await axios.patch(
+        `http://localhost:3000/api/notes/${noteId}`,
+        {
+          title: newTitle,
+          description: newDescription,
+        },
+      );
+      console.log(data);
+      fetchNotes();
+    };
+    updateNote();
+  };
+
   useEffect(() => {
-    information();
+    fetchNotes();
   }, []);
 
   return (
-    <div>
+    <div className="p-4">
+      <form
+        onSubmit={submitHandler}
+        className="flex gap-5 flex-wrap px-3 py-5"
+        action=""
+      >
+        <input
+          className="w-80 border border-black p-2 rounded"
+          type="text"
+          name="title"
+          id=""
+        />
+        <input
+          className="w-80 border border-black p-2 rounded"
+          type="text"
+          name="description"
+          id=""
+        />
+        <button className="px-5 py-2.5 bg-gray-700 text-white rounded active:scale-95">
+          Create Note
+        </button>
+      </form>
+
       <div className="notes p-2 flex gap-5">
         {notes.map((note) => {
           return (
-            <div className="note bg-gray-600 text-white p-4 w-fit rounded-2xl">
+            <div className="note bg-gray-600 text-white p-4 w-fit rounded-2xl text-center">
               <h1 className="text-3xl font-bold">{note.title}</h1>
               <p className="text-xl">{note.description}</p>
+              <div className="flex gap-5">
+                <button
+                  onClick={() => {
+                    deleteHandler(note._id);
+                  }}
+                  className="px-5 py-2.5 bg-white text-black rounded-2xl mt-2 active:scale-95"
+                >
+                  Delete Note
+                </button>
+                <button
+                  onClick={() => {
+                    updateHandler(note._id);
+                  }}
+                  className="px-5 py-2.5 bg-white text-black rounded-2xl mt-2 active:scale-95"
+                >
+                  Update Note
+                </button>
+              </div>
             </div>
           );
         })}
